3202c8fde4693ccee8723d7374609d278e6a2c61
Jessica Leigh <jleigh@pietech.com>
2017-11-16 00:21:04 -0500

    added images and formatting

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,28 @@
 
 var randomnum = [];
-var numgiven ="";
+var numgiven = "";
+var bluenum = Math.floor(Math.random() * (11 - 1 + 1)) + 1;
+var greennum = Math.floor(Math.random() * (11 - 1 + 1)) + 1;
+var rednum = Math.floor(Math.random() * (11 - 1 + 1)) + 1;
+var yellownum = Math.floor(Math.random() * (11 - 1 + 1)) + 1;
+var total = "";
 
-function startGame(){
-
+function startGame()
+{
 	var numgiven = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
 	    
 	randomnum.push(numgiven);
 
-	console.log(numgiven);
+	console.log(randomnum);
 
 	document.getElementById("randomnum").innerHTML = numgiven;
-
 }	
+
+$('#blue').on ('click', function()
+{
+	document.getElementById("score").innerHTML = bluenum;
+	console.log(total);
+})
+
+
 
